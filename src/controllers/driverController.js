b03fa87d471b8a3f338bb649ed85f4c493d442c1
Justin Hager <developerjustinhager@pm.me>
2021-06-30 15:02:28 -0700

    add edit driver route and controller

--- src/controllers/driverController.js
+++ src/controllers/driverController.js
@@ -17,4 +17,27 @@ module.exports = {
       .then((driver) => res.status(201).send(driver))
       .catch(next);
   },
+  edit(req, res, next) {
+    const driverId = req.params.id;
+    const driverProps = req.body;
+
+    if (driverProps.email === undefined && driverProps.driving === undefined) {
+      return res.status(422).json({
+        error: { message: `Missing valid fields in request body` },
+      });
+    }
+
+    const updateProps = {};
+    if (driverProps.email) {
+      updateProps.email = driverProps.email;
+    }
+    if (driverProps.driving) {
+      updateProps.driving = driverProps.driving;
+    }
+
+    Driver.findByIdAndUpdate({ _id: driverId }, updateProps)
+      .then(() => Driver.findById({ _id: driverId }))
+      .then((driver) => res.status(201).send(driver))
+      .catch(next);
+  },
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,4 +3,5 @@ const DriversController = require('../controllers/driverController');
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
   app.post('/api/drivers', DriversController.create);
+  app.put('/api/drivers/:id', DriversController.edit);
 };
