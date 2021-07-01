54bbfe09106491cf177d9f8aac989e94b1ae6551
Justin Hager <developerjustinhager@pm.me>
2021-07-01 07:32:17 -0700

    add Point schema to Driver model

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const DriversController = require('../controllers/driverController');
 
 module.exports = (app) => {
   app.get('/api', DriversController.greeting);
+  app.get('/api/drivers', DriversController.index);
   app.post('/api/drivers', DriversController.create);
   app.put('/api/drivers/:id', DriversController.edit);
   app.delete('/api/drivers/:id', DriversController.delete);
